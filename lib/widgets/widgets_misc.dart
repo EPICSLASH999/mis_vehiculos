1cd942fc3ed1cb39f5bc69e74a10c32239bca455
Jesus Santos <jesus.santos364@gmail.com>
2023-12-12 16:35:42 -0700

    Si CuadroDeTexto esDouble solo permite 2 decimales.

--- lib/widgets/widgets_misc.dart
+++ lib/widgets/widgets_misc.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mis_vehiculos/blocs/bloc.dart';
 import 'package:mis_vehiculos/extensiones/extensiones.dart';
 import 'package:mis_vehiculos/variables/variables.dart';
+import 'dart:math' as math;
 
 /* -------------------------------- COMPONENTES -------------------------------- */
 class TituloComponente extends StatelessWidget {
@@ -124,6 +126,7 @@ class CuadroDeTexto extends StatelessWidget {
             decoration: obtenerDecoracion(),
             keyboardType: obtenerTipoTeclado(),
             autofocus: focusTecaldo,
+            inputFormatters: !esDouble?null:[DecimalTextInputFormatter(decimalRange: 2)],
             onTap: () { 
               if(esSoloLectura) return;
               if(!esPrimerClic) return;
@@ -134,6 +137,44 @@ class CuadroDeTexto extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+// Clase para que TextField solo permita 2 decimales
+class DecimalTextInputFormatter extends TextInputFormatter {
+  DecimalTextInputFormatter({required this.decimalRange})
+      : assert(decimalRange > 0);
+
+  final int decimalRange;
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue, // unused.
+    TextEditingValue newValue,
+  ) {
+    TextSelection newSelection = newValue.selection;
+    String truncated = newValue.text;
+
+    String value = newValue.text;
+
+    if (value.contains(".") &&
+        value.substring(value.indexOf(".") + 1).length > decimalRange) {
+      truncated = oldValue.text;
+      newSelection = oldValue.selection;
+    } else if (value == ".") {
+      truncated = "0.";
+
+      newSelection = newValue.selection.copyWith(
+        baseOffset: math.min(truncated.length, truncated.length + 1),
+        extentOffset: math.min(truncated.length, truncated.length + 1),
+      );
+    }
+
+    return TextEditingValue(
+      text: truncated,
+      selection: newSelection,
+      composing: TextRange.empty,
+    );
+    //return newValue;
   }
 }
 
